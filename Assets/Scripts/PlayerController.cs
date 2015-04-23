7adf96aab2c7198c0f413c18619dcd3dab5ce4b8
CorPruijs <cor@pruys.com>
2015-04-24 00:16:50 +0200

    Added scripts, sprites

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float VerticalInput = Input.GetAxis("Vertical");
+	
+	}
+}
